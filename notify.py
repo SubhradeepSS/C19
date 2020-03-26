6625517a5c432e1cc217dd79588fd475461187e3
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-03-26 11:33:41 +0530

    Completed (scheduled)desktop notification system

--- notify.py
+++ notify.py
@@ -1,6 +1,38 @@
 from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+import time
 
 
 def notify(title, message):
     notification.notify(title=title, message=message,
                         app_icon="C:\\Users\HP\Desktop\Covid-19 Desktop Notification\icon.ico", timeout=10)
+
+
+def get_data_from_url(url):
+    return requests.get(url).text
+
+
+if __name__ == '__main__':
+    while True:
+        HTML_data = get_data_from_url('https://www.mohfw.gov.in/')
+        # print(HTML_data)
+        soup = BeautifulSoup(HTML_data, 'html.parser')
+        #   print(soup)
+        data_str = ''
+        for tr in soup.find_all('tbody')[7].find_all('tr'):
+            data_str += tr.get_text()
+        data_str = data_str[1:]
+        data_list = data_str.split('\n\n')
+        # statewise_info_list = []
+        states = ['Bihar', 'Delhi', 'West Bengal']  # States for which notification is required
+        for data in data_list[0:len(data_list) - 5]:
+            statewise_info_list = data.split('\n')
+            if statewise_info_list[1] in states:
+                print(statewise_info_list)
+                title = 'Covid19'
+                message = f'State - {statewise_info_list[1]}\nConfirmed(Indian) - {statewise_info_list[2]} ; Confirmed(Foreign) - {statewise_info_list[3]} ; Cured - {statewise_info_list[4]} ; Death - {statewise_info_list[5]}'
+                notify(title, message)
+                time.sleep(2)
+
+        time.sleep(3600)  # Notification after every 1 hour (3600 secs)
