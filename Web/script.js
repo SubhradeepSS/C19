daeebae0cdb9173fd90482708c0e0766f682943c
Subhradeep Saha <subhradeep11012000@gmail.com>
2020-06-05 01:01:11 +0530

    [feat]: Timeseries data chart

--- Web/script.js
+++ Web/script.js
@@ -16,24 +16,54 @@
 
 let stats = document.getElementById('stats');
 const url = "http://covid19-india-adhikansh.herokuapp.com/states";
-const netStatURL = "http://covid19-india-adhikansh.herokuapp.com/summary";
+// const netStatURL = "https://covid-19india-api.herokuapp.com/all";
+const netStatURL = "https://api.covidindiatracker.com/total.json";
+// const newURL = "https://covid-19india-api.herokuapp.com/v2.0/country_data";
 
 let netStats = document.getElementById('netStats');
 
 function update() {
 
     fetch(netStatURL)
-    .then(response => response.json())
-    .then(data => {
-        let activeT = data["Active cases"];
-        let curedT = data["Cured/Discharged/Migrated"];
-        let deathT = data["Death"];
-        let totalT = data["Total Cases"]
-        netStats.innerHTML = `<span class="badge badge-danger">Active: ${activeT}</span>
-                                <span class="badge badge-success">Cured: ${curedT}</span>
-                                <span class="badge badge-secondary">Death: ${deathT}</span>
-                                <span class="badge badge-dark">Total: ${totalT}</span>`
-    })
+        .then(response => response.json())
+        .then(data => {
+            // let active_cases = data[1].active_cases;
+            // let active_rate = data[1].active_rate;
+            // let confirmed_cases = data[1].confirmed_cases;
+            // let death_cases = data[1].death_cases;
+            // let death_rate = data[1].death_rate;
+            // let delta_change_active_cases = data[1].delta_change_active_cases;
+            // let delta_change_death_cases = data[1].delta_change_death_cases;
+            // let delta_change_recovered_cases = data[1].delta_change_recovered_cases;
+            // let migrated_cases = data[1].migrated_cases;
+            // let recovered_cases = data[1].recovered_cases;
+            // let recovered_rate = data[1].recovered_rate;
+            // let last_updated = data[1].last_updated;
+
+            // netStats.innerHTML = `<span class="badge badge-danger">Active: ${active_cases}<br>+${delta_change_active_cases} @${active_rate}%</span>
+            //                     <span class="badge badge-success">Recovered: ${recovered_cases}<br>+${delta_change_recovered_cases} @${recovered_rate}%</span>
+            //                     <span class="badge badge-secondary" >Death: ${death_cases}<br>+${delta_change_death_cases} @${death_rate}</span>
+            //                     <span class="badge badge-dark>Total: ${confirmed_cases}</span>
+            //                     <span class="badge badge-dark>Migrated: ${migrated_cases}</span>
+            //                     Last updates: ${last_updated}`
+            //                     ;
+            // console.log(data);
+            let active = data["active"];
+            let confirmed = data["confirmed"];
+            let recovered = data["recovered"];
+            let deaths = data["deaths"];
+            let achanges = data["achanges"];
+            let cchanges = data["cchanges"];
+            let rchanges = data["rchanges"];
+            let dchanges = data["dchanges"];
+
+            netStats.innerHTML = `<span class="badge badge-danger my-3 mx-3">Active:<br>${active}(+${achanges})</span>
+                                <span class="badge badge-success my-3 mx-3">Recovered:<br>${recovered}(+${rchanges})</span>
+                                <span class="badge badge-secondary my-3 mx-3">Death:<br>${deaths}(+${dchanges})</span>
+                                <span class="badge badge-dark my-3 mx-3">Total:<br>${confirmed}(+${cchanges})</span>
+                                `;
+        })
+
 
     fetch(url)
         .then(response => response.json())
@@ -46,11 +76,11 @@ function update() {
                 let total = state.total;
 
                 // statesList.push(name);
-                
+
                 // let html = "";
                 stats.innerHTML += `<div class="card my-3 mx-3" style="width: 18rem;">
                                         <div class="card-header">
-                                            <b>${name}</b>
+                                            <h3>${name}</h3>
                                         </div>
                                         <ul class="list-group list-group-flush">
                                             <li class="list-group-item Active">Active: ${active}</li>
@@ -72,11 +102,11 @@ searchState.addEventListener('input', () => {
     let inputText = searchState.value.toLowerCase();
     let stateCard = document.getElementsByClassName('card');
     Array.from(stateCard).forEach(element => {
-        let state = element.getElementsByTagName('b')[0].innerText.toLowerCase();
-        if(state.includes(inputText)){
+        let state = element.getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if (state.includes(inputText)) {
             element.style.display = "block";
         }
-        else{
+        else {
             element.style.display = "none";
         }
     })
---
+++ Web/timeseries/script.js
@@ -0,0 +1,70 @@
+let my_chart;
+function axesLinearChart(cases_list, recovered_list, deaths_list, dates) {
+
+    if (my_chart) {
+        my_chart.destroy();
+    }
+    let graph = document.getElementById('graph').getContext("2d");
+    my_chart = new Chart(graph, {
+        type: 'line',
+        data: {
+            datasets: [{
+                label: 'Confirmed',
+                data: cases_list,
+                fill: false,
+                borderColor: '#FFF',
+                backgroundColor: '#FFF',
+                borderWidth: 1
+            }, {
+                label: 'Recovered',
+                data: recovered_list,
+                fill: false,
+                borderColor: '#009688',
+                backgroundColor: '#009688',
+                borderWidth: 1
+            }, {
+                label: 'Deaths',
+                data: deaths_list,
+                fill: false,
+                borderColor: '#f44336',
+                backgroundColor: '#f44336',
+                borderWidth: 1
+            }],
+            labels: dates
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        fontSize: 10
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        fontSize: 10
+                    }
+                }]
+            },
+        }
+    });
+}
+
+const url = "https://api.covid19india.org/data.json";
+
+fetch(url)
+    .then(response => response.json())
+    .then(DATA => {
+        // console.log(data);
+        let data = DATA.cases_time_series;
+        let confirmed = [], death = [], recovered = [], date = [];
+
+        data.forEach(element => {
+            confirmed.push(parseInt(element.totalconfirmed));
+            death.push(parseInt(element.totaldeceased));
+            recovered.push(parseInt(element.totalrecovered));
+            date.push(element.date);
+        });
+        axesLinearChart(confirmed, recovered, death, date);
+    })
